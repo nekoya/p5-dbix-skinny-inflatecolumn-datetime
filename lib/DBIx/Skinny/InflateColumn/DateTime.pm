0d9c2b0d8bd183a9f2dae9c89581b8c6868885c1
Ryo Miyake <ryo.studiom@gmail.com>
2009-10-18 18:30:47 +0900

    initial commit

---
+++ lib/DBIx/Skinny/InflateColumn/DateTime.pm
@@ -0,0 +1,33 @@
+package DBIx::Skinny::InflateColumn::DateTime;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+DBIx::Skinny::InflateColumn::DateTime -
+
+=head1 SYNOPSIS
+
+  use DBIx::Skinny::InflateColumn::DateTime;
+
+=head1 DESCRIPTION
+
+DBIx::Skinny::InflateColumn::DateTime is
+
+=head1 AUTHOR
+
+Default Name E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
