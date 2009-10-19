8109a18a6e5906620731baf978645a1c4642b764
Ryo Miyake <ryo.studiom@gmail.com>
2009-10-19 22:12:13 +0900

    wrote Pod

--- lib/DBIx/Skinny/InflateColumn/DateTime.pm
+++ lib/DBIx/Skinny/InflateColumn/DateTime.pm
@@ -50,21 +50,78 @@ __END__
 
 =head1 NAME
 
-DBIx::Skinny::InflateColumn::DateTime -
+DBIx::Skinny::InflateColumn::DateTime - Auto inflate/deflate controller for DBIx::Skinny
 
 =head1 SYNOPSIS
 
+Use this module in your schema.
+
+  package Your::DB::Schema;
+  use DBIx::Skinny::Schema;
   use DBIx::Skinny::InflateColumn::DateTime;
+
+  install_table table1 => {
+      pk 'id';
+      columns qw/id name created_at updated_at/;
+  };
+
+  install_table table2 => {
+      pk 'id';
+      columns qw/id name booked_on created_on updated_on/;
+  };
+
+In your app.
+
+  my $row = Your::DB->single('table1', { id => 1 });
+  print $row->created_at->ymd;  # created_at is DateTime object
 
 =head1 DESCRIPTION
 
-DBIx::Skinny::InflateColumn::DateTime is
+DBIx::Skinny::InflateColumn::DateTime provides inflate/deflate settings for *_at/*_on columns.
+
+It also set trigger for pre_insert and pre_update.
+
+=head1 INFLATE/DEFLATE
+
+This module installs inflate rule for /_(at|on)$/ columns.
+
+That columns will be inflated as DateTime objects.
+
+=head1 TRIGGERS
+
+=head2 pre_insert
+
+Set current timestamp for created_at, created_on, updated_at and updated_on if column exists.
+
+=head2 pre_update
+
+Set current timestamp for updated_at and updated_on if column exists.
+
+B<CAUTION:> Following code does not work like you expects.
+  my $row = Your::DB->single('table1', { id => 1 });
+  $row->update({ name => 'updated' });
+  print "updated at: " . $row->updated_at;
+
+Because DBIx::Skinny does not fetch values that modified in triggers when update row object.
+
+If you want to get real updated_at value, you should fetch row again.
+
+  my $row = Your::DB->single('table1', { id => 1 });
+  $row->update({ name => 'updated' });
+  $row = Your::DB->single('table1', { id => 1 });
+  print "updated at: " . $row->updated_at;
 
 =head1 AUTHOR
 
-Default Name E<lt>default {at} example.comE<gt>
+Ryo Miyake E<lt>ryo.studiom {at} gmail.comE<gt>
 
 =head1 SEE ALSO
+
+DBIx::Skinny, DBIx::Class::InflateColumn::DateTime
+
+=head1 AUTHOR
+
+Ryo Miyake  C<< <ryo.studiom@gmail.com> >>
 
 =head1 LICENSE
 
