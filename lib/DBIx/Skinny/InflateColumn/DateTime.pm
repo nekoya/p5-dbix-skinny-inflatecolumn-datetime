3c5d5fd368fca3f032b4f9f30c5db26171a0b1e0
Ryo Miyake <ryo.studiom@gmail.com>
2009-10-19 18:57:19 +0900

    implemented

--- lib/DBIx/Skinny/InflateColumn/DateTime.pm
+++ lib/DBIx/Skinny/InflateColumn/DateTime.pm
@@ -4,6 +4,47 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use DateTime;
+use DateTime::Format::Strptime;
+use DateTime::Format::MySQL;
+use DateTime::TimeZone;
+
+sub import {
+    my $timezone = DateTime::TimeZone->new(name => 'local');
+    my $schema = caller;
+    for my $rule ( qw(^.+_at$ ^.+_on$) ) {
+        $schema->inflate_rules->{ $rule }->{ inflate } = sub {
+            my $value = shift or return;
+            return $value if ref $value eq 'DateTime';
+            my $dt = DateTime::Format::Strptime->new(
+                pattern   => '%Y-%m-%d %H:%M:%S',
+                time_zone => $timezone,
+            )->parse_datetime($value);
+            return DateTime->from_object( object => $dt );
+        };
+        $schema->inflate_rules->{ $rule }->{ deflate } = sub {
+            my $value = shift;
+            return DateTime::Format::MySQL->format_datetime($value);
+        };
+    }
+    my $schema_info = $schema->schema_info;
+    my $now = DateTime->now(time_zone => $timezone);
+    push @{ $schema->common_triggers->{ pre_insert } }, sub {
+        my ($self, $args, $table) = @_;
+        my $columns = $schema_info->{ $table }->{ columns };
+        for my $key ( qw/created_at created_on updated_at updated_on/ ) {
+            $args->{$key} ||= $now if grep {/^$key$/} @$columns;
+        }
+    };
+    push @{ $schema->common_triggers->{ pre_update } }, sub {
+        my ($self, $args, $table) = @_;
+        my $columns = $schema_info->{ $table }->{ columns };
+        for my $key ( qw/updated_at updated_on/ ) {
+            $args->{$key} ||= $now if grep {/^$key$/} @$columns;
+        }
+    };
+}
+
 1;
 __END__
 
